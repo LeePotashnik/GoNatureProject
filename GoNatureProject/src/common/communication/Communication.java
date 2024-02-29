ee6139efda6b613a1040d08e31de9757bddb633c
eladkrauz <elad.krauz@e.braude.ac.il>
2024-02-29 15:04:30 +0200

    Communication is Serializable

--- GoNatureProject/src/common/communication/Communication.java
+++ GoNatureProject/src/common/communication/Communication.java
@@ -1,5 +1,12 @@
 package common.communication;
 
-public class Communication {
+import java.io.Serializable;
+
+/**
+ * A class for the communications between client-side to server-side Includes
+ * all the elements required to transmit a valid message that is Serializable
+ */
+public class Communication implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 }
