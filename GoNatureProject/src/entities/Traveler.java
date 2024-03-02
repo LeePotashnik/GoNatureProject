ab9e69e241528f301efdc5a0d37bcbebef36b41c
ofirBerkovitsh <ofirbrko@gmail.com>
2024-03-02 23:18:34 +0200

    try traveler

--- GoNatureProject/src/entities/Traveler.java
+++ GoNatureProject/src/entities/Traveler.java
@@ -5,6 +5,6 @@ import java.time.LocalTime;
 public class Traveler extends ParkVisitor {
 	public Traveler(String idNumber, String firstName, String lastName, String username, String password,
 			String emailAddress, String phoneNumber, boolean isLoggedIn, LocalTime lastLogIn, VisitorType visitorType) {
-		super(idNumber, firstName, lastName, username, password, emailAddress, phoneNumber, isLoggedIn, lastLogIn, visitorType);
+		super(idNumber, firstName, lastName, username, password, emailAddress, phoneNumber, isLoggedIn, visitorType);
 	}
 }
