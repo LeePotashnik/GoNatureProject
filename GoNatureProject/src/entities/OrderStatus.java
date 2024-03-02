987e003b5d47399b378bbad126fbf8459a1a85c4
Uriziv15 <Uri.Ziv@e.braude.ac.il>
2024-03-02 22:57:53 +0200

    ofir meatzbenet

---
+++ GoNatureProject/src/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package entities;
+
+public enum OrderStatus {
+	PENDING, CONFIRMED, CANCELLED, INWAITINGLIST, DONE;
+}
---
+++ GoNatureProject/src/entities/SystemUser.java
@@ -0,0 +1,101 @@
+package entities;
+
+import java.time.LocalTime;
+
+public class SystemUser {
+	private String idNumber, firstName, lastName, username, password, emailAddress, phoneNumber;
+	private boolean isLoggedIn;
+	private LocalTime lastLogIn;
+	
+	public SystemUser(String idNumber, String firstName, String lastName, String username, String password,
+			String emailAddress, String phoneNumber, boolean isLoggedIn, LocalTime lastLogIn) {
+		this.idNumber = idNumber;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.emailAddress = emailAddress;
+		this.phoneNumber = phoneNumber;
+		this.isLoggedIn = isLoggedIn;
+		this.lastLogIn = lastLogIn;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public LocalTime getLastLogIn() {
+		return lastLogIn;
+	}
+
+	public boolean isLoggedIn() {
+		return isLoggedIn;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setLoggedIn(boolean isLoggedIn) {
+		this.isLoggedIn = isLoggedIn;
+	}
+	
+	public void setLastLogIn(LocalTime lastLogIn) {
+		this.lastLogIn = lastLogIn;
+	}
+
+	@Override
+	public String toString() {
+		return "SystemUser [idNumber=" + idNumber + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", username=" + username + ", password=" + password + ", emailAddress=" + emailAddress
+				+ ", phoneNumber=" + phoneNumber + ", isLoggedIn=" + isLoggedIn + ", lastLogIn=" + lastLogIn + "]";
+	}
+}
