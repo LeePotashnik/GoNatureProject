037b64f5d9448adf77178c22ab4bf82a511d84e8
Uriziv15 <Uri.Ziv@e.braude.ac.il>
2024-03-17 23:28:45 +0200

    finishing payment screen

--- GoNatureProject/src/clientSide/gui/PaymentSystemScreenController.java
+++ GoNatureProject/src/clientSide/gui/PaymentSystemScreenController.java
@@ -1,9 +1,6 @@
 package clientSide.gui;
 
-import java.time.Month;
-import java.time.format.TextStyle;
 import java.util.ArrayList;
-import java.util.Locale;
 
 import common.controllers.AbstractScreen;
 import common.controllers.ScreenException;
@@ -29,7 +26,6 @@ import javafx.util.Pair;
 
 public class PaymentSystemScreenController extends AbstractScreen {
 	private Booking booking;
-	private ParkVisitor visitor;
 	private Pair<Booking, ParkVisitor> pair;
 
 	@FXML
@@ -45,12 +41,17 @@ public class PaymentSystemScreenController extends AbstractScreen {
 	@FXML
 	private ComboBox<String> yearComboBox;
 
+	/////////////////////////////
+	/// ACTION EVENTS METHODS ///
+	/////////////////////////////
+	
 	/**
-     * Processes the payment when the 'Process Payment' button is clicked.
-     * Validates the payment information and navigates to the confirmation screen if validation passes.
-     *
-     * @param event The action event that triggers the payment process.
-     */
+	 * Processes the payment when the 'Process Payment' button is clicked. Validates
+	 * the payment information and navigates to the confirmation screen if
+	 * validation passes.
+	 *
+	 * @param event The action event that triggers the payment process.
+	 */
 	@FXML
 	void processPayment(ActionEvent event) {
 		boolean valid = paymentValidation();
@@ -65,25 +66,76 @@ public class PaymentSystemScreenController extends AbstractScreen {
 		}
 	}
 
-	/**
-     * Returns to the previous screen.
-     *
-     * @param event The action event that triggers the return.
-     * @throws ScreenException if there is an issue navigating back.
-     * @throws StatefulException if there is a state management issue during navigation.
-     */
-	@FXML
-	void returnToPreviousScreen(ActionEvent event) throws ScreenException, StatefulException {
-		ScreenManager.getInstance().goToPreviousScreen(false, true);
+//	/**
+//	 * Returns to the previous screen.
+//	 *
+//	 * @param event The action event that triggers the return.
+//	 * @throws ScreenException   if there is an issue navigating back.
+//	 * @throws StatefulException if there is a state management issue during
+//	 *                           navigation.
+//	 */
+//	@FXML
+//	void returnToPreviousScreen(ActionEvent event) throws ScreenException, StatefulException {
+//		ScreenManager.getInstance().goToPreviousScreen(false, true);
+//
+//	}
+	
+	///////////////////////////////////
+	/// JAVAFX FLOW CONTROL METHODS ///
+	///////////////////////////////////
 
+	// Methods for handling tab key presses in various text fields and ComboBoxes.
+	// These ensure that focus moves logically through the payment form.
+	@FXML
+	void cvvTabPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.TAB) {
+			event.consume();
+			holderNameTxt.requestFocus();
+		}
 	}
 
+	@FXML
+	void holderNameTabPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.TAB) {
+			event.consume();
+			processPayment.requestFocus();
+		}
+	}
+
+	@FXML
+	void lastCardNumberTabPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.TAB) {
+			event.consume();
+			monthComboBox.requestFocus();
+		}
+	}
+
+	@FXML
+	void monthTabPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.TAB) {
+			event.consume();
+			yearComboBox.requestFocus();
+		}
+	}
+
+	@FXML
+	void yearTabPressed(KeyEvent event) {
+		if (event.getCode() == KeyCode.TAB) {
+			event.consume();
+			cvvTxt.requestFocus();
+		}
+	}
+	
+	////////////////////////
+	/// INSTANCE METHODS ///
+	////////////////////////
+
 	/**
-     * Validates all payment information fields for correct format and completeness.
-     * Highlights any fields with invalid input.
-     *
-     * @return true if all input fields are valid, false otherwise.
-     */
+	 * Validates all payment information fields for correct format and completeness.
+	 * Highlights any fields with invalid input.
+	 *
+	 * @return true if all input fields are valid, false otherwise.
+	 */
 	public boolean paymentValidation() {
 		cardNumber1Txt.setStyle(setFieldToRegular());
 		cardNumber2Txt.setStyle(setFieldToRegular());
@@ -120,17 +172,17 @@ public class PaymentSystemScreenController extends AbstractScreen {
 		if (!cardValid) {
 			showMessage += "\n- Credit card number is not valid.";
 		}
-		 // Validate ComboBox selections
-	    if (!validateComboBoxSelection(monthComboBox)) {
-	        yearComboBox.setStyle(setFieldToError()); 
+		// Validate ComboBox selections
+		if (!validateComboBoxSelection(monthComboBox)) {
+			yearComboBox.setStyle(setFieldToError());
 			showMessage += "\n- Month is not valid.";
-	        valid = false;
-	    }
-	    if (!validateComboBoxSelection(yearComboBox)) {
-	        yearComboBox.setStyle(setFieldToError()); 
+			valid = false;
+		}
+		if (!validateComboBoxSelection(yearComboBox)) {
+			yearComboBox.setStyle(setFieldToError());
 			showMessage += "\n- Year is not valid.";
-	        valid = false;
-	    }
+			valid = false;
+		}
 		// Validate CVV: non-empty and exactly 3 digits
 		if (cvvTxt.getText() == null || cvvTxt.getText().length() != 3) {
 			cvvTxt.setStyle(setFieldToError());
@@ -150,57 +202,39 @@ public class PaymentSystemScreenController extends AbstractScreen {
 	}
 
 	/**
-     * Validates that a card number field contains exactly 4 digits.
-     *
-     * @param cardField The text field containing the card number to be validated.
-     * @return true if the field is valid, false otherwise.
-     */
+	 * Validates that a card number field contains exactly 4 digits.
+	 *
+	 * @param cardField The text field containing the card number to be validated.
+	 * @return true if the field is valid, false otherwise.
+	 */
 	private boolean validateCardField(TextField cardField) {
 		return cardField.getText() != null && cardField.getText().matches("\\d{4}");
 	}
-	
-	 /**
-     * Validates that a ComboBox has a selection made.
-     *
-     * @param comboBox The ComboBox to validate.
-     * @return true if a selection has been made, false otherwise.
-     */
-	private boolean validateComboBoxSelection(ComboBox<?> comboBox) {
-	    return comboBox.getValue() != null;
-	}
 
 	/**
-     * Initializes the controller class. This method is called after the FXML file has been loaded.
-     * Sets up the month and year ComboBoxes and formats the card number and CVV text fields.
-     */
-	@Override
-	public void initialize() {
-//		updateAmountLabel();
-		populateMonthComboBox();
-		populateYearComboBox();
-		setupTextField(cardNumber1Txt, cardNumber2Txt);
-		setupTextField(cardNumber2Txt, cardNumber3Txt);
-		setupTextField(cardNumber3Txt, cardNumber4Txt);
-		setupTextField(cardNumber4Txt, null);
-		cvvTxt.textProperty().addListener((observable, oldValue, newValue) -> {
-			if (!newValue.matches("\\d*")) { // If new value is not a digit
-				cvvTxt.setText(newValue.replaceAll("[^\\d]", "")); // Remove non-digits
-			} else if (newValue.length() > 3) { // If new value has more than 3 digits
-				cvvTxt.setText(newValue.substring(0, 3)); // Truncate to 3 digits
-			}
-		});
-	}
-
-	/**
-	 * Configures a text field for credit card input, enforcing numerical input and automatic focus management.
-	 * This method ensures that each text field only accepts digits and automatically moves the focus to the next field
-	 * once the user has entered 4 digits, facilitating a smooth and efficient input process for the credit card number.
-	 * If the text field is the last in the sequence (i.e., nextField is null), it does not attempt to move focus further.
+	 * Validates that a ComboBox has a selection made.
 	 *
-	 * @param currentField The text field currently being configured. This field will accept only digit characters
-	 *                     and is limited to a maximum of 4 digits.
-	 * @param nextField The next text field to focus on once the currentField is fully populated. If currentField
-	 *                  is the last in the sequence, this parameter should be null.
+	 * @param comboBox The ComboBox to validate.
+	 * @return true if a selection has been made, false otherwise.
+	 */
+	private boolean validateComboBoxSelection(ComboBox<?> comboBox) {
+		return comboBox.getValue() != null;
+	}
+
+	/**
+	 * Configures a text field for credit card input, enforcing numerical input and
+	 * automatic focus management. This method ensures that each text field only
+	 * accepts digits and automatically moves the focus to the next field once the
+	 * user has entered 4 digits, facilitating a smooth and efficient input process
+	 * for the credit card number. If the text field is the last in the sequence
+	 * (i.e., nextField is null), it does not attempt to move focus further.
+	 *
+	 * @param currentField The text field currently being configured. This field
+	 *                     will accept only digit characters and is limited to a
+	 *                     maximum of 4 digits.
+	 * @param nextField    The next text field to focus on once the currentField is
+	 *                     fully populated. If currentField is the last in the
+	 *                     sequence, this parameter should be null.
 	 */
 	private void setupTextField(TextField currentField, TextField nextField) {
 		currentField.textProperty().addListener(new ChangeListener<String>() {
@@ -222,96 +256,87 @@ public class PaymentSystemScreenController extends AbstractScreen {
 			}
 		});
 	}
-	
-//	public void updateAmountLabel() {
-//		amountLbl.setText(String.valueOf(booking.getFinalPrice()) + "$"); // Update the label's text programmatically
-//    }
+
+	public void updateAmountLabel() {
+		amountLbl.setText(String.valueOf(booking.getFinalPrice()) + "$"); // Update the label's text programmatically
+    }
 
 	/**
-     * Populates the month ComboBox with month numbers.
-     */
+	 * Populates the month ComboBox with month numbers.
+	 */
 	private void populateMonthComboBox() {
-	    ObservableList<String> months = FXCollections.observableArrayList();
-	    for (int i = 1; i <= 12; i++) {
-	        months.add(String.format("%02d", i)); // Formats the month as two digits
-	    }
-	    monthComboBox.setItems(months); // Set all items at once
+		ObservableList<String> months = FXCollections.observableArrayList();
+		for (int i = 1; i <= 12; i++) {
+			months.add(String.format("%02d", i)); // Formats the month as two digits
+		}
+		monthComboBox.setItems(months); // Set all items at once
 	}
 
 	/**
-     * Populates the year ComboBox with years from the current year up to 20 years into the future.
-     */
+	 * Populates the year ComboBox with years from the current year up to 20 years
+	 * into the future.
+	 */
 	private void populateYearComboBox() {
-	    int currentYear = java.time.Year.now().getValue();
-	    
-	    ArrayList<String> years = new ArrayList<>();
-	    for (int i = currentYear; i <= currentYear + 20; i++) {
-	        years.add(String.valueOf(i)); // Add as string if your ComboBox type is String
-	    }
-	    yearComboBox.setItems(FXCollections.observableArrayList(years)); // Set all items at once
+		int currentYear = java.time.Year.now().getValue();
+
+		ArrayList<String> years = new ArrayList<>();
+		for (int i = currentYear; i <= currentYear + 20; i++) {
+			years.add(String.valueOf(i)); // Add as string if your ComboBox type is String
+		}
+		yearComboBox.setItems(FXCollections.observableArrayList(years)); // Set all items at once
+	}
+	
+	///////////////////////////////
+	/// ABSTRACT SCREEN METHODS ///
+	///////////////////////////////
+
+	/**
+	 * Initializes the controller class. This method is called after the FXML file
+	 * has been loaded. Sets up the month and year ComboBoxes and formats the card
+	 * number and CVV text fields.
+	 */
+	@Override
+	public void initialize() {
+		populateMonthComboBox();
+		populateYearComboBox();
+		setupTextField(cardNumber1Txt, cardNumber2Txt);
+		setupTextField(cardNumber2Txt, cardNumber3Txt);
+		setupTextField(cardNumber3Txt, cardNumber4Txt);
+		setupTextField(cardNumber4Txt, null);
+		cvvTxt.textProperty().addListener((observable, oldValue, newValue) -> {
+			if (!newValue.matches("\\d*")) { // If new value is not a digit
+				cvvTxt.setText(newValue.replaceAll("[^\\d]", "")); // Remove non-digits
+			} else if (newValue.length() > 3) { // If new value has more than 3 digits
+				cvvTxt.setText(newValue.substring(0, 3)); // Truncate to 3 digits
+			}
+		});
 	}
 
-	
 	/**
-     * Loads booking and visitor information before the screen is displayed.
-     *
-     * @param information The booking and visitor information passed to the screen.
-     */
+	 * Loads booking and visitor information before the screen is displayed.
+	 *
+	 * @param information The booking and visitor information passed to the screen.
+	 */
 	@SuppressWarnings("unchecked")
 	@Override
 	public void loadBefore(Object information) {
 		if (information != null && information instanceof Pair) {
 			pair = (Pair<Booking, ParkVisitor>) information;
 			booking = pair.getKey();
-			visitor = pair.getValue();
+			updateAmountLabel();
+
 		}
+
 	}
 
 	/**
-     * Returns the title of the screen.
-     *
-     * @return A string representing the title of the Payment Screen.
-     */
+	 * Returns the title of the screen.
+	 *
+	 * @return A string representing the title of the Payment Screen.
+	 */
 	@Override
 	public String getScreenTitle() {
 		return "Payment Screen";
 	}
-	
-	// Methods for handling tab key presses in various text fields and ComboBoxes.
-    // These ensure that focus moves logically through the payment form.
-	@FXML
-    void cvvTabPressed(KeyEvent event) {
-		if (event.getCode() == KeyCode.TAB) {
-			event.consume();
-			holderNameTxt.requestFocus();
-		}
-    }
-    @FXML
-    void holderNameTabPressed(KeyEvent event) {
-    	if (event.getCode() == KeyCode.TAB) {
-			event.consume();
-			processPayment.requestFocus();
-		}
-    }
-    @FXML
-    void lastCardNumberTabPressed(KeyEvent event) {
-    	if (event.getCode() == KeyCode.TAB) {
-			event.consume();
-			monthComboBox.requestFocus();
-		}
-    }
-    @FXML
-    void monthTabPressed(KeyEvent event) {
-    	if (event.getCode() == KeyCode.TAB) {
-			event.consume();
-			yearComboBox.requestFocus();
-		}
-    }
-    @FXML
-    void yearTabPressed(KeyEvent event) {
-    	if (event.getCode() == KeyCode.TAB) {
-			event.consume();
-			cvvTxt.requestFocus();
-		}
-    }
+
 }
