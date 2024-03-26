d7fd4a9965e882f06062d1facddb5e3154f136f8
Lee Potashnik <Lee Potashnik@DESKTOP-HCHTV4T>
2024-03-27 00:15:32 +0200

    Disconnecting the client when it is not connected as a user Update - employees and visitors

--- GoNatureProject/src/clientSide/gui/ParkVisitorAccountScreenController.java
+++ GoNatureProject/src/clientSide/gui/ParkVisitorAccountScreenController.java
@@ -161,7 +161,8 @@ public class ParkVisitorAccountScreenController extends AbstractScreen {
 			if (userControl.logoutUser())
 				parkVisitor.setLoggedIn(false);
 			try {
-				ScreenManager.getInstance().goToPreviousScreen(false, false);
+				ScreenManager.getInstance().showScreen("MainScreenController",
+						"/clientSide/fxml/MainScreen.fxml", true, false, null);
 			} catch (ScreenException | StatefulException e) {
 				e.printStackTrace();
 			}
